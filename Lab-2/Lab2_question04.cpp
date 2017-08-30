ef7d91efb78801b85f9dcc7ab5bec74802cdc2ec
Ayush Singh <ayush.singh@niser.ac.in>
2017-08-30 21:43:18 +0530

    Improved Lab2_question04.cpp

--- Lab-2/Lab2_question04.cpp
+++ Lab-2/Lab2_question04.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main(){
-	float days, weeks, years;
+	int days, weeks, years, wdays, ymonths, ydays;
 
 	cout << "Enter the number of days: ";
 	cin >> days;
 
 	weeks = days/7;
-	years = days/365;
+	wdays = days%7;
 
-	cout << days << "days is equivalent to " << weeks << " weeks or " << years << " years." << endl;
-	
+	years = days/365;
+	ymonths = (days%365)/30;
+	ydays = ymonths%30;
+
+	cout << days << " days is equivalent to " << weeks << " weeks and " << wdays << " days." << endl;
+	cout << days << " days is equivalent to " << years << " years, " << ymonths << " months and " << ydays << " days." << endl;
+
 	return 0;
 }
